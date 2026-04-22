ce07161af745b012063f3e2511efa08587d8b9a5
swaraj_sankar <swaraj.sankar@codepoints.in>
2026-04-22 15:38:43 +0530

    fix

--- android/app/src/main/kotlin/com/example/screenshare/SystemAudioCaptureService.kt
+++ android/app/src/main/kotlin/com/example/screenshare/SystemAudioCaptureService.kt
@@ -1,31 +1,51 @@
 package com.example.screenshare
 
 import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
 import android.media.AudioFormat
 import android.media.AudioPlaybackCaptureConfiguration
 import android.media.AudioRecord
-import android.media.MediaRecorder
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
 import android.os.Build
 import android.os.IBinder
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.Result
 
 @RequiresApi(Build.VERSION_CODES.Q)
 class SystemAudioCaptureService : Service() {
 
     private var audioRecord: AudioRecord? = null
-    private var recordingThread: Thread? = null
     private var isRecording = false
+
+    private lateinit var mediaProjectionManager: MediaProjectionManager
+    private var mediaProjection: MediaProjection? = null
+    private var resultCode: Int = 0
+    private var dataIntent: Intent? = null
 
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onCreate() {
         super.onCreate()
         startForegroundServiceNotification()
+
+        mediaProjectionManager =
+            getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        resultCode = intent?.getIntExtra("resultCode", Activity.RESULT_OK) ?: Activity.RESULT_OK
+        dataIntent = intent?.getParcelableExtra("data")
+
+        mediaProjection =
+            mediaProjectionManager.getMediaProjection(resultCode, dataIntent!!)
+
         startAudioCapture()
+
+        return START_STICKY
     }
 
     private fun startForegroundServiceNotification() {
@@ -51,11 +71,9 @@ class SystemAudioCaptureService : Service() {
     }
 
     private fun startAudioCapture() {
-        val mediaProjection =
-            mediaProjectionManager.getMediaProjection(resultCode, dataIntent)
 
-        val config = AudioPlaybackCaptureConfiguration.Builder(mediaProjection)
-            .addMatchingUsage(android.media.AudioAttributes.USAGE_MEDIA)
+        val config = AudioPlaybackCaptureConfiguration.Builder(mediaProjection!!)
+            .addMatchingUsage(AudioAttributes.USAGE_MEDIA)
             .build()
 
         val bufferSize = AudioRecord.getMinBufferSize(
@@ -75,6 +93,9 @@ class SystemAudioCaptureService : Service() {
             .setBufferSizeInBytes(bufferSize)
             .setAudioPlaybackCaptureConfig(config)
             .build()
+
+        audioRecord?.startRecording()
+        isRecording = true
     }
 
     override fun onDestroy() {
